dfe12667bc1222454244117c4950fa6461167860
Ramon de Lemos <ramondelemos@gmail.com>
2024-07-10 16:06:14 -0300

    chore: Redirect to original repo

--- build_deps.sh
+++ build_deps.sh
@@ -9,9 +9,9 @@ CPUS=`getconf _NPROCESSORS_ONLN 2>/dev/null || sysctl -n hw.ncpu`
 # https://github.com/confluentinc/librdkafka.git
 
 LIBRDKAFKA_DESTINATION=librdkafka
-LIBRDKAFKA_REPO=https://github.com/ramondelemos/librdkafka.git
+LIBRDKAFKA_REPO=https://github.com/confluentinc/librdkafka.git
 LIBRDKAFKA_BRANCH=master
-LIBRDKAFKA_REV=6eaf89fb124c421b66b43b195879d458a3a31f86
+LIBRDKAFKA_REV=9b72ca3aa6c49f8f57eea02f70aadb1453d3ba1f
 LIBRDKAFKA_SUCCESS=src/librdkafka.a
 
 # https://github.com/cameron314/concurrentqueue.git
